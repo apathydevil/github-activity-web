cacbaad1e7c68af975781e8877c3bb77f42ad409
apathydevil <76252555+apathydevil@users.noreply.github.com>
2026-03-28 18:23:19 +0100

    Add activity route and form for username input in index.html

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+@app.route('/activity')
+def activity():
+    username = request.args.get('username')
+    return f"Showing activity for {username}"
 
 if __name__ == '__main__':
     app.run(debug=True)
